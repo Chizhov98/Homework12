fa633908eaa516cd9042ba79e439be79bdd0896e
Chizhov98 <chizhov1998a@gmail.com>
2021-06-06 17:44:41 +0300

    Task2

---
+++ src/Demo.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Demo {
+    public static String read(String path) {
+
+        try {
+//открываем файл по пути
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentString;
+//построчно считываем файл
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append(" \n ");
+            }
+//закрываем файл
+            reader.close();
+//возвращаем вычитанный текст в строке
+            return stringBuilder.toString();
+        } catch (IOException e ) {
+            return "File not found";
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(read("C:\\test.txt"));
+    }
+}
